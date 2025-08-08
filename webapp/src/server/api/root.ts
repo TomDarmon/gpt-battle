0cf056301033d280fc7d3800255d7066637b4853
tomdarmon-appstack <tom@app-stack.tech>
2025-08-08 17:56:58 +0200

    fix: clean remove dead code

--- webapp/src/server/api/root.ts
+++ webapp/src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { matchRouter } from "~/server/api/routers/match";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +7,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   match: matchRouter,
 });
 
